c8b4a1652513deef4c40293716039753c2c6f10e
Mr a <101287880+Arashvscode@users.noreply.github.com>
2022-12-19 23:42:12 +0330

    Update CustomAdGhostWeb.cpp
    add new final Style for GhostWeb

--- app/src/main/java/Ninja/coder/Ghostemane/code/io/github/rosemoe/sora/widget/CustomAdGhostWeb.cpp
+++ app/src/main/java/Ninja/coder/Ghostemane/code/io/github/rosemoe/sora/widget/CustomAdGhostWeb.cpp
@@ -20,7 +20,7 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import com.google.android.material.textview.MaterialTextView;
 import java.util.Locale;
 import io.github.rosemoe.sora.widget.EditorColorScheme;
 import Ninja.coder.Ghostemane.code.R;
@@ -30,72 +30,58 @@ import java.util.HashMap;
 
 @SuppressWarnings("CanBeFinal")
 class CustomAdGhostWeb extends EditorCompletionAdapter {
-	private HashMap<String, Object> imap = new HashMap<>();
-	
+
 	@Override
 	public int getItemHeight() {
 		// 45 dp
-		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60,
-		getContext().getResources().getDisplayMetrics());
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 45,
+				getContext().getResources().getDisplayMetrics());
 	}
-	
+
 	@Override
 	public View getView(int pos, View view, ViewGroup parent, boolean isCurrentCursorPosition) {
 		if (view == null) {
-			view = LayoutInflater.from(getContext()).inflate(R.layout.autotextruner, parent, false);
+			view = LayoutInflater.from(getContext()).inflate(R.layout.oneui, parent, false);
 		}
 		CompletionItem item = getItem(pos);
-		TextView tv = view.findViewById(R.id.res);
-		TextView tv1 = view.findViewById(R.id.tv1);
-		
+		MaterialTextView tv = view.findViewById(R.id.res);
+		MaterialTextView tv1 = view.findViewById(R.id.tv1);
+		LinearLayout display = view.findViewById(R.id.display);
 		LinearLayout mob = view.findViewById(R.id.mob);
-		LinearLayout Ninjaback = view.findViewById(R.id.Ninjaback);
-		imap = new HashMap<>();
 		var label = Spannable.Factory.getInstance().newSpannable(item.label);
 		var prefix = getPrefix();
-		
 		var index = TextUtils.indexOf(item.label.toLowerCase(Locale.ROOT), prefix.toLowerCase(Locale.ROOT));
 		if (index != -1) {
-			label.setSpan(new ForegroundColorSpan(0xFFFF9193), index, index + prefix.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-			
-		} else {
-			
-			
-			
+			label.setSpan(new ForegroundColorSpan(0xFFFF9193), index, index + prefix.length(),
+					Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 		}
 		tv.setText(label);
-		
-		//	view.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)3, 0xFF171717));
-		//	mob.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)3, 0xFF171717));
+		AnimUtils.O(view);
 		tv = view.findViewById(R.id.iogore);
+		tv1.setTextColor(Color.WHITE);
 		tv.setText(item.desc);
-		
-		
 		tv1.setText(item.desc.subSequence(0, 1));
-		
-		view.setTag(pos);
-		if (isCurrentCursorPosition) {
-			
-			Ninjaback.setBackground(new GradientDrawable() {
-				public GradientDrawable getIns(int a, int b) {
-					this.setCornerRadius(a);
-					this.setColor(b);
-					return this;
-				}
-			}.getIns((int) 16, 0xFF181822));
-			
-		} else {
-			
-			Ninjaback.setBackground(new GradientDrawable() {
-				public GradientDrawable getIns(int a, int b) {
-					this.setCornerRadius(a);
-					this.setColor(b);
-					return this;
-				}
-			}.getIns((int) 15, 0xFF181822));
-			
+		String lastItemPost = tv1.getText().toString();
+		if (lastItemPost.endsWith("H")) {
+			SetBackGroundItem(Color.parseColor("#FF6CF550"), display);
+		} else if (lastItemPost.endsWith("Q")) {
+		} else if (lastItemPost.endsWith("C")) {
+			SetBackGroundItem(Color.parseColor("#FFFF6F15"), display);
+		} else if (lastItemPost.endsWith("K")) {
+			SetBackGroundItem(Color.parseColor("#FFB1014D"), display);
+		} else if (lastItemPost.endsWith("P")) {
+			SetBackGroundItem(Color.parseColor("#FFA614FB"), display);
+		} else if (lastItemPost.endsWith("J")) {
+			SetBackGroundItem(Color.parseColor("#FFFF4D5A"), display);
 		}
+		view.setTag(pos);
 		return view;
 	}
-	
+
+	protected void SetBackGroundItem(int colors, View view) {
+		GradientDrawable pos = new GradientDrawable();
+		pos.setColor(colors);
+		pos.setCornerRadius((float) 18);
+		view.setBackground(pos);
+	}
 }
